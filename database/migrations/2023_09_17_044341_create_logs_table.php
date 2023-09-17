622aff26df0e6a55b1783f6d7adf434f59d73eb6
Krisna Dana <krisnadanaofficial@gmail.com>
2023-09-17 13:35:43 +0800

    done api

---
+++ database/migrations/2023_09_17_044341_create_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedDouble('input');
+            $table->unsignedDouble('hasil');
+            $table->unsignedDouble('waktu');
+            $table->enum('jenis', ['API', 'PL-SQL']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
